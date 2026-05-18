a37eaf6535cf70c85a5311c2d31cd908a7d77a70
Federica Barrera <fede@MacBook-Air-de-Feder.local>
2026-05-18 12:22:49 -0500

    version final

--- punteros+arreglos+funciones/ejercicios1.c
+++ punteros+arreglos+funciones/ejercicios1.c
@@ -10,6 +10,16 @@ int main()
         printf("%.2f", notas[i]);
     }
 
-    calcularPromedio(notas,tamanio);
+    printf("El promedio es: %.2f", calcularPromedio(notas,tamanio));
+}
 
+float calcularPromedio(float *vector, int tamanio)
+{
+    float suma, promedio=0; 
+    for (int i = 0; i < tamanio; i++)
+    {
+        suma = suma + vector[i];
+    }
+    promedio = suma / tamanio;
+    return promedio;
 }
