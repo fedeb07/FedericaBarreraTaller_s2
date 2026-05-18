4a398498d254b3b7878eb1f14728a4155b116fd7
Federica Barrera <fede@MacBook-Air-de-Feder.local>
2026-05-18 12:13:46 -0500

    definicion de prototipo

--- punteros+arreglos+funciones/ejercicios1.c
+++ punteros+arreglos+funciones/ejercicios1.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+float calcularPromedio(float *vector, int tamanio);
 int main()
 {
-    float v[5]={6,7,7.5,6.5,8};
+    float notas[5]={6,7,7.5,6.5,8};
+    int tamanio = 5;
 
     for (int i = 0; i < 5; i++)
     {
-        printf("%.1f", v[i]);
+        printf("%.2f", notas[i]);
     }
+
+    calcularPromedio(notas,tamanio);
+
 }
